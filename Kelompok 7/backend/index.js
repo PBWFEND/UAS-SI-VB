61dc77893334c9c9647428c398efa24683cb96ce
deynaangelyzahara <230660221032@student.unsap.ac.id>
2026-01-15 06:50:23 +0700

    Menambahkan Project UAS Kelompok 7

---
+++ Kelompok 7/backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+// 1. Import Routes (Menghubungkan ke jalur yang dipisah)
+const authRoutes = require('./routes/authRoutes');
+const transaksiRoutes = require('./routes/transaksiRoutes');
+
+const app = express();
+
+// 2. Middleware Utama
+app.use(cors()); 
+app.use(express.json()); 
+
+// 3. Daftar Endpoint API 
+// Login dan Register 
+app.use('/api/auth', authRoutes); 
+// CRUD Transaksi 
+app.use('/api/transaksi', transaksiRoutes); 
+
+// 4. Cek Status Server 
+app.get('/', (req, res) => {
+    res.json({
+        message: "Aplikasi Keuangan Pribadi Aktif!",
+        status: "Ready"
+    });
+});
+
+// 5. Konfigurasi Port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`==========================================`);
+    console.log(`Server Berhasil Dijalankan!`);
+    console.log(`URL Backend: http://localhost:${PORT}`);
+    console.log(`==========================================`);
+});
