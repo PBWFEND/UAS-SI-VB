d346641ed7a34a20acc8b334cadb3a4e0c557dd7
230660221100 <230660221100@student.unsap.ac.id>
2026-01-15 20:36:16 +0700

    modifikasi frontend api.js

--- uasbend/uas-frontend/src/services/api.js
+++ uasbend/uas-frontend/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:3000/api", // SESUAIKAN PORT BACKEND
 });
 
-api.interceptors.request.use(config => {
+// AUTO PASANG TOKEN
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
