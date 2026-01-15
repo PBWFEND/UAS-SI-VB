7f0118f1c506432cbf0c54b6dbcd6f18caf7c6e8
deynaangelyzahara <230660221032@student.unsap.ac.id>
2026-01-15 10:51:26 +0700

    fix: cors allow all origins

--- Kelompok 7/backend/index.js
+++ Kelompok 7/backend/index.js
@@ -12,10 +12,12 @@ app.use(express.json());
 
 // Konfigurasi CORS yang lebih fleksibel untuk Vercel
 app.use(cors({
-  origin: true, 
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.options('*', cors());
 
 // Daftar Endpoint API
 app.use('/api/auth', authRoutes);
